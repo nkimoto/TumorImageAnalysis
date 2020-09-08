4317047eafaaa096d8247623bdf5b81312b9aa0d
nkimoto <kimoppy126@gmail.com>
2020-09-08 07:27:45 +0000

    Add argparse to split_images script

--- split_images.py
+++ split_images.py
@@ -1,5 +1,6 @@
 #! coding: utf-8
 
+import sys
 import os
 import cv2
 from matplotlib import pyplot as plt
@@ -8,22 +9,16 @@ import pandas as pd
 from PIL import Image, ImageFile
 from logzero import logger
 import pickle
+import argparse 
 
 
 REF_FILE = "AI_data20200812.xlsx"
 LABEL_DIR_NAME = "0.05x_seg_to_0.5x"
 SAMPLE_DIR_NAME = "HE_10x_exported_0.5x"
-Image.MAX_IMAGE_PIXELS = 1000000000
-# more than this value is processed as case data
-# STAIN = 'ER'
-# threshold = 0
-# STAIN = 'PgR'
-# threshold = 0
-STAIN = 'Ki-67'
-threshold = 17
+Image.MAX_IMAGE_PIXELS = 10000000000
 
 
-def label_sample_generator(ref, stain=STAIN):
+def label_sample_generator(ref, stain='ER', threshold=90):
     for l, s in zip(
         sorted(os.listdir(LABEL_DIR_NAME)), sorted(os.listdir(SAMPLE_DIR_NAME))
     ):
@@ -74,10 +69,20 @@ def convert_PIL_to_array(img):
     return np.asarray(img)
 
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-t', '--threshold', help='The threshold of immunostaining result (default: 90)', type=float, default=90) 
+    parser.add_argument('-s', '--stain', help='The type of immunostaining (default: ER)', type=str,
+                        choices=['ER', 'PgR', 'Ki-67'], default='ER') 
+    args = parser.parse_args()  
+    return args
+
+
+
+def main(stain='ER', threshold=90):
     ImageFile.LOAD_TRUNCATED_IMAGES = True
     ref = pd.read_excel(REF_FILE)
-    gen = label_sample_generator(ref)
+    gen = label_sample_generator(ref, stain=stain, threshold=threshold)
     control, case = {}, {}
     im_dict = {"case": {}, "control": {}}
     logger.info("Start Processing!")
@@ -104,4 +109,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args.stain, args.threshold)
